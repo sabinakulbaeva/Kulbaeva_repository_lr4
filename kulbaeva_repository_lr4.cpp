81b98ca5d8fa78fcc478ad74b72fca0ef3f577a9
sabinakulbaeva <sabinakulbaeva1@mail.ru>
2025-04-07 10:59:59 +0300

    empty fix

---
+++ kulbaeva_repository_lr4.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// Прототипы методов
+void inputNumbers(double& x, double& y, double& z);
+double sumFractionalParts(double x, double y, double z);
+int sumIntegerParts(double x, double y, double z);
+void displayMenu();
+char getUserChoice();
+
+int main() {
+    // Переменные для хранения введенных чисел
+    double x, y, z;
+    
+    while (true) {
+        displayMenu(); // Отображение меню
+
+        char choice = getUserChoice(); // Получаем выбор пользователя
+
+        switch(choice) {
+            case '1':
+                inputNumbers(x, y, z); // Ввод чисел
+                break;
+            case '2':
+                cout << "Сумма дробных частей: " << sumFractionalParts(x, y, z) << endl;
+                break;
+            case '3':
+                cout << "Сумма целых частей: " << sumIntegerParts(x, y, z) << endl;
+                break;
+            case '0':
+                return 0; // Выход из программы
+            default:
+                cout << "Неверный ввод! Попробуйте снова." << endl;
+                break;
+        }
+    }
+}
+
+// Функция ввода чисел
+void inputNumbers(double& x, double& y, double& z) {
+    //реализуется Василием
+}
+
+// Функция нахождения суммы дробных частей
+double sumFractionalParts(double x, double y, double z) {
+    //реализуется Натальей
+}
+
+// Функция нахождения суммы целых частей
+int sumIntegerParts(double x, double y, double z) {
+    //реализуется Ваней
+}
+
+// Функция отображения меню
+void displayMenu() {
+   //реализуется Никитой
+}
+
+// Функция получения выбора пользователя
+char getUserChoice() {
+   //реализуется Сашей
+}
